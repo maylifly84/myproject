c49df87207c4953b8ccdcaad3e5399d3c9be63ec
maylifly84 <maylifly84@users.noreply.github.com>
2019-02-12 17:55:00 +0100

    s5

--- TheSailorTeam/js/script.js
+++ TheSailorTeam/js/script.js
@@ -1,3 +1,11 @@
-$(window).scroll(function(){
-    $('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
-});
+let img = document.querySelectorAll('.image')
+  
+for (let i = 0; i < 9; i++) {
+  img[i].addEventListener('click', () => {
+    if (img[i].classList.contains('focus') == false) {
+        img[i].classList.add('focus');
+    } else {
+        img[i].classList.remove('focus');
+    }
+  });
+}
